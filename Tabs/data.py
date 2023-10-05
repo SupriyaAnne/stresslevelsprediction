2a33a68cfba36194877e62dd66176ce7f8e2d022
SupriyaAnne <146002496+SupriyaAnne@users.noreply.github.com>
2023-10-06 00:46:45 +0530

    Add files via upload

---
+++ Tabs/data.py
@@ -0,0 +1,33 @@
+import streamlit as st
+def app(df):
+    st.title("Data Information page")
+    st.subheader("View Data")
+    with st.expander("View data"):
+        st.dataframe(df)
+    st.subheader("Columns Description:")
+    if st.checkbox("View Summary"):
+        st.dataframe(df.describe())
+    col_name, col_dtype, col_data = st.columns(3)
+    with col_name:
+        if st.checkbox("Column Names"):
+            st.dataframe(df.columns)
+    with col_dtype:
+        if st.checkbox("Columns data types"):
+            dtypes = df.dtypes.apply(lambda x: x.name)
+            st.dataframe(dtypes)
+    with col_data:
+        if st.checkbox("Columns Data"):
+            col = st.selectbox("Column Name", list(df.columns))
+            st.dataframe(df[col])
+    st.markdown("""
+                    <p style="font-size:24px">
+                        <a 
+                            href="https://www.kaggle.com/datasets/laavanya/human-stress-detection-in-and-through-sleep?
+                            select=SaYoPillow.csv"
+                            target=_blank
+                            style="text-decoration:none;"
+                        >Get Dataset
+                        </a> 
+                    </p>
+                """, unsafe_allow_html=True
+                )
---
+++ Tabs/visualise.py
@@ -0,0 +1,62 @@
+import warnings
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+def app(df, X, y):
+    warnings.filterwarnings('ignore')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.title("Visualise the Stress Level")
+
+    if st.checkbox("Show the correlation heatmap"):
+        st.subheader("Correlation Heatmap")
+        # Create a correlation heatmap using Plotly
+        fig = px.imshow(df.corr())
+        st.plotly_chart(fig)
+
+    if st.checkbox("Show Scatter Plot"):
+        st.subheader("Scatter Plots")
+        # Create scatter plots using Plotly Express
+        scatter_fig = px.scatter(df, x='bt', y='rr', title="Body Temperature vs Respiration Rate")
+        scatter_fig2 = px.scatter(df, x='sr', y='lm', title="Snoring Rate vs Limb Movement")
+        scatter_fig3 = px.scatter(df, x='bo', y='bt', title="Blood Oxygen vs Body Temperature")
+        scatter_fig4 = px.scatter(df, x='sh', y='hr', title="Sleeping Hours vs Heart Rate")
+
+        # Display the scatter plots
+        st.plotly_chart(scatter_fig)
+        st.plotly_chart(scatter_fig2)
+        st.plotly_chart(scatter_fig3)
+        st.plotly_chart(scatter_fig4)
+
+    if st.checkbox("Show 3D Scatter Plot"):
+        st.subheader("3D Scatter Plot")
+        # Create a 3D scatter plot using Plotly Express
+        scatter_3d_fig = px.scatter_3d(df, x='bt', y='rr', z='hr', title="3D Scatter Plot")
+        st.plotly_chart(scatter_3d_fig)
+
+    if st.checkbox("Display Boxplot"):
+        st.subheader("Box Plots")
+        # Create box plots using Plotly Express
+        box_fig = px.box(df, y=['sr', 'rr', 'bt', 'rem', 'bo', 'sh'], title="Box Plot of Features")
+
+        # Display the box plot
+        st.plotly_chart(box_fig)
+
+    if st.checkbox("Show Histogram"):
+        st.subheader("Histograms")
+        # Create histograms using Plotly Express
+        hist_fig = px.histogram(df, x='bt', title="Body Temperature Histogram")
+        hist_fig2 = px.histogram(df, x='hr', title="Heart Rate Histogram")
+
+        # Display the histograms
+        st.plotly_chart(hist_fig)
+        st.plotly_chart(hist_fig2)
+
+    if st.checkbox("Show Violin Plot"):
+        st.subheader("Violin Plot")
+        # Create a violin plot using Plotly Express
+        violin_fig = px.violin(df, y='rr', box=True, points="all", title="Violin Plot")
+        st.plotly_chart(violin_fig)
+
+
